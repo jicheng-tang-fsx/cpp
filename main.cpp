58574c2941c02bb46e365f14bee7fa85f0c97e02
jicheng-tang-fsx <jicheng.tang@finstadiumx.co.jp>
2024-06-09 10:34:56 +0900

    update 3

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 std::generator<int> Fibonacci(int max) {
     int a = 0, b = 1;
     while (a <= max) {
-        co_yield a;  // 使用 co_yield 返回当前的斐波那契数字
+        co_yield a;
         auto next = a + b;
         a = b;
         b = next;
@@ -14,7 +14,6 @@ std::generator<int> Fibonacci(int max) {
 }
 
 int main() {
-    // 打印不超过15的斐波那契数
     for (auto n : Fibonacci(15)) {
         std::cout << n << " ";
     }
